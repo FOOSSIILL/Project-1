3d047832b6c7fb222d23000417a6c104f66fa552
SofíaMG <98538133+FOOSSIILL@users.noreply.github.com>
2023-09-19 12:56:38 -0500

    dist

---
+++ src/Data.ts
@@ -0,0 +1,15 @@
+const data = [
+    {
+        id: 1,
+        name: "Panquesitos MG",
+        username: "panquesitosMG",
+        email: "Panquesitos@april.biz",
+        description: {
+            name: "Chocolate cake",
+            ingredients: {
+                step1: "3 cups flour (420 grams)",
+                step2: "",
+            },
+        },
+    },
+]
